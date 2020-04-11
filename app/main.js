47128882d05b2de4489ab8246f259706d3c31f53
Fritz179 <Fritz179@users.noreply.github.com>
2020-04-11 17:43:39 +0200

    Update
    Colore logo
    Spostato cambio lingua
    cambiato immagini in azienda
    cambiato immagine in servizi
    Rimosso categorie in galleria

--- app/main.js
+++ app/main.js
@@ -6,7 +6,7 @@ const path = require('path')
 require('./setup/express.js')(app, __dirname);
 
 app.get('/', (req, res) => {
-  res.render('home/index.ejs', {dir: 'home'})
+  res.redirect('/azienda')
 })
 
 app.get('/fa-solid.woff2', (req, res) => {
@@ -15,10 +15,8 @@ app.get('/fa-solid.woff2', (req, res) => {
 
 const fotoNums = {
   stufe: [21, 6, 14, 25, 29, 28, 2, 27],
-  bagno: [19, 7, 10, 11, 12, 15, 16, 17, 18, 22, 20, 3],
-  piastrelle: [18, 24, 3, 4, 23, 5, 13, 1, 7, 6, 12, 21, 10, 22, 25, 19, 9, 8, 17, 0],
-  resina: [16, 26, 20, 11, 15],
-  tutto: [18, 24, 3, 4, 23, 5, 13, 1, 7, 6, 12, 21, 28, 29, 14, 10, 22, 25, 19, 9, 8, 17, 0, 2, 11, 15, 16, 20, 26, 27]
+  piastrelle: [18, 24, 3, 4, 23, 5, 13, 1, 7, 12, 10, 22, 19, 9, 8, 17, 0],
+  resina: [16, 26, 20, 11, 15]
 }
 
 app.get('/galleria/:filter', (req, res) => {
@@ -27,7 +25,7 @@ app.get('/galleria/:filter', (req, res) => {
 })
 
 app.get('/galleria', (req, res) => {
-  res.render('galleria/index.ejs', {cards: fotoNums.tutto, filter: 'tutto', dir: 'galleria'})
+  res.render('galleria/index.ejs', {cards: fotoNums.piastrelle, filter: 'piastrelle', dir: 'galleria'})
 });
 
 app.get('/galerie/:filter', (req, res) => {
@@ -36,10 +34,10 @@ app.get('/galerie/:filter', (req, res) => {
 })
 
 app.get('/galerie', (req, res) => {
-  res.render('galerie/index.ejs', {cards: fotoNums.tutto, filter: 'tutto', dir: 'galerie'})
+  res.render('galerie/index.ejs', {cards: fotoNums.piastrelle, filter: 'piastrelle', dir: 'galerie'})
 });
 
-['de', 'azienda', 'firma', 'azienda_old', 'contatti', 'kontakte', 'servizi', 'dienstleistungen', 'concessionarie', 'hauser'].forEach(dir => {
+['azienda', 'firma', 'contatti', 'kontakte', 'servizi', 'dienstleistungen', 'concessionarie', 'hauser'].forEach(dir => {
   app.get(`/${dir}`, (req, res) => {
     res.render(`${dir}/index.ejs`, {dir})
   })
